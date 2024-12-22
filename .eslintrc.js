d440e7ba59e98155dd695efd79fbd55cc6348b70
Vituli <130680989+Vituli@users.noreply.github.com>
2024-12-22 13:52:42 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,19 +18,26 @@ module.exports = {
     'jest'
   ],
   rules: {
-    '@typescript-eslint/restrict-template-expressions': ['error', { allowAny: true }]
+    // Existing rule
+    '@typescript-eslint/restrict-template-expressions': ['error', { allowAny: true }],
+
+    // Example: turn off strict-boolean-expressions
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+
+    // Example: lower severity for no-unnecessary-type-assertion
+    '@typescript-eslint/no-unnecessary-type-assertion': 'warn'
   },
   settings: {
     react: {
       version: 'detect'
     }
   },
-  overrides: [{
-    files: [
-      '**/*.stories.*'
-    ],
-    rules: {
-      'import/no-anonymous-default-export': 'off'
+  overrides: [
+    {
+      files: ['**/*.stories.*'],
+      rules: {
+        'import/no-anonymous-default-export': 'off'
+      }
     }
-  }]
+  ]
 }
